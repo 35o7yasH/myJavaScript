c39f9032ee98ac91e12457e4008ff57a0e2b9d71
35o7yasH <95373145+35o7yasH@users.noreply.github.com>
2024-02-12 18:36:26 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"user strict" //treats all the code as newer version, but in modern days we don't have to actually write this too.
+
+//aleart ("booo, it's pop-up box."); //while runnning it in node there's some error to be faced, but in case of running it in the developers console there's no errors to be faced.
+
+/*
+//console.log(3+3); console.log("hello");
+  the code readability is gone now.
+console.log(3 + 
+    3)
+*/
+console.log("Hello world!");
+
+// dataTypes
+
+let name = "nitin" //strings
+let age = 25 //numbers
+let isLoggedIn = true/false //boolean
+let state = null
+let me;
+
+
+// numbers = 2^53 that's the range of the numbers in js
+// bigInt
+// string = ""
+// boolean = true/false
+// null = standalone value
+// undefined =
+// symbol = used when ever we need something unique
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
