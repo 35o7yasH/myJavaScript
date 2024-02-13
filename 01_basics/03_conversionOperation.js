371287acb90f7e3824e6ac3b39b41b3a9531c3a9
35o7yasH <95373145+35o7yasH@users.noreply.github.com>
2024-02-13 12:29:12 +0000

    omg

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -31,5 +31,55 @@ let someNumber = 33;
 
 let valueInString = String(33);
 
-console.log(valueInString); //33
-console.log(typeof valueInString); //string
+// console.log(valueInString); //33
+// console.log(typeof valueInString); //string
+
+//******************************************  Operation  *********************************************** */
+
+
+let value = 3;
+let negValue = -value;
+
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**3);
+// console.log(2%2);
+
+
+
+
+let str1 = "Hello"
+let str2 = " Nitin"
+let newStr = str1 + str2;
+console.log(newStr);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+
+
+// console.log(+true);
+// console.log(+"");
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2;
+
+//this all are bad code, 
+
+
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
