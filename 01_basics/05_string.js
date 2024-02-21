5d27fe324786c85a14a99902d2c31b047faadc01
35o7yasH <95373145+35o7yasH@users.noreply.github.com>
2024-02-21 14:41:54 +0000

    to_be

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -4,3 +4,8 @@ const repoCount = 6
 // console.log(name + repoCount + " Value");
 
 console.log(`Hello my name is ${name}, and my repo count is ${repoCount}.`);
+
+const gameName = new String ('nitinhgfc');
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
