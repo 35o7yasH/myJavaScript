8e22ccb03b4b37cfcddfa1a2fa36601bf88f2f07
35o7yasH <95373145+35o7yasH@users.noreply.github.com>
2024-02-22 13:40:29 +0000

    strings_done

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -5,7 +5,7 @@ const repoCount = 6
 
 // console.log(`Hello my name is ${name}, and my repo count is ${repoCount}.`);
 
-const gameName = new String ('nitin-hcfcb');
+const gameName = new String ('nitin-hc-fcb');
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
@@ -15,15 +15,28 @@ const gameName = new String ('nitin-hcfcb');
 // console.log(gameName.toUpperCase());
 
 
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('t'));
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
 
 
 
-const newString = gameName.substring(0, 5);
-console.log(newString);
+// const newString = gameName.substring(0, 5);
+// console.log(newString);
 
 
-const anotherString = gameName.slice
+// const anotherString = gameName.slice(-10, 4);
+// console.log(anotherString);
 
 
+// const trimmedString = "      yash      ";
+// console.log(trimmedString);
+// console.log(trimmedString.trim());
+
+
+const url = "https://nitin@hcfcb%20karmakar";
+
+console.log(url.replace('%20', '-')); // https://nitin@hcfcb-karmakar
+
+console.log(url.includes('yash')); // false
+
+console.log(gameName.split('-')); // [nitin, hc, fcb]
