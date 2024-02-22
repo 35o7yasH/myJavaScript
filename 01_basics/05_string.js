19ab34ab20b4b67059e11c63f9663ba24cc712f5
35o7yasH <95373145+35o7yasH@users.noreply.github.com>
2024-02-22 11:35:06 +0000

    continue_hoga

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -3,9 +3,27 @@ const repoCount = 6
 
 // console.log(name + repoCount + " Value");
 
-console.log(`Hello my name is ${name}, and my repo count is ${repoCount}.`);
+// console.log(`Hello my name is ${name}, and my repo count is ${repoCount}.`);
 
-const gameName = new String ('nitinhgfc');
+const gameName = new String ('nitin-hcfcb');
 
-console.log(gameName[0]);
-console.log(gameName.__proto__);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+
+
+const newString = gameName.substring(0, 5);
+console.log(newString);
+
+
+const anotherString = gameName.slice
+
+
