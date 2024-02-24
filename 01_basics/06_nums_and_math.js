16818546bbc498038d9768afd873bad5024ae36b
35o7yasH <95373145+35o7yasH@users.noreply.github.com>
2024-02-24 08:01:09 +0000

    i'll be continuing this

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,9 @@
+const score = 500;
+// console.log(score);
+
+const balance = new Number(100);
+console.log(balance);
+
+console.log(balance.toString.length);
+console.log(balance.toFixed(2));
+console.log(balance.toFixed(1));
