1c63a0c9a717a23ada1904dd8742717dd3382ac1
35o7yasH <95373145+35o7yasH@users.noreply.github.com>
2024-02-26 20:25:37 +0000

    maths and nums

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -2,8 +2,41 @@ const score = 500;
 // console.log(score);
 
 const balance = new Number(100);
-console.log(balance);
+// console.log(balance);
 
-console.log(balance.toString.length);
-console.log(balance.toFixed(2));
-console.log(balance.toFixed(1));
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+// console.log(balance.toFixed(1));
+
+
+const otherNumber = 23.648
+// console.log(otherNumber.toPrecision(3));
+
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+
+//++++++++++++++++++++++++++++++++++++++/ Maths /++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// console.log(Math);
+// console.log(Math.abs(-69.96)); // abs is absolute value which makes megative value to positive value.
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.8));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log((Math.random()*10) + 1);
+console.log(Math.floor((Math.random()*10 + 1)));
+
+
+const max = 20;
+const min = 10;
+
+
+console.log(Math.floor(Math.random() * ((max - min) + 1)) + min);
