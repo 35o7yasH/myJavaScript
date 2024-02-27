ce26555f3a29d4f0c22c420b941c9dc1401dd0d1
35o7yasH <95373145+35o7yasH@users.noreply.github.com>
2024-02-27 10:09:01 +0000

    date_&_time-in_JS

---
+++ 01_basics/07_datesinJS.js
@@ -0,0 +1,46 @@
+//Dates
+
+
+let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toDateString());
+// console.log(typeof myDate);
+
+
+
+//Lets try to print a specific date now.
+
+
+// let myCreatedDate = new Date(2023, 0, 26);// taking an random date
+// console.log(myCreatedDate);
+// console.log(myCreatedDate.toDateString());
+
+
+// to print the time of an random date 
+// let myNewCreatedDate = new Date (2023, 0, 26, 5, 3);
+// let myNewCreatedDate = new Date ("2023-01-26");
+let myNewCreatedDate = new Date ("01-26-2023");
+// console.log(myNewCreatedDate.toLocaleString());
+
+
+
+// let myTimeStamp = new Date().getTime() // the other way
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myNewCreatedDate.getTime());
+
+// console.log(Date.now()/1000);
+// console.log(Math.floor(Date.now()/1000));
+
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getDate());
+// console.log(newDate.getMonth() + 1);
+
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+})
