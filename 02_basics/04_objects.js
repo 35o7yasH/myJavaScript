eacf77e538b19ed3fc0eeb5300dfe9316b242d79
35o7yasH <95373145+35o7yasH@users.noreply.github.com>
2024-03-02 11:14:14 +0000

    continue hoga

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,21 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "nitin"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+
+const regularUser = {
+    email: " my@gmail.com",
+    fullName: {
+        userFullName: {
+            firstName: "Nitin",
+            lastName: "Choudary"
+        }
+    }
+}
+
+// console.log(regularUser.fullName.userFullName["lastName"]);
