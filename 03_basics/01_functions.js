806962477fc1e9ec984ea3f78f9e4193c60df416
35o7yasH <95373145+35o7yasH@users.noreply.github.com>
2024-03-04 16:24:45 +0000

    i'll be finishing it by today

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,21 @@
+// const nitin = function(){
+//     console.log("Hello world!");
+
+// }
+// console.log(typeof nitin);
+
+function sayMyName() {
+    console.log('N');
+console.log('i');
+console.log('t');
+console.log('i');
+console.log('n');
+
+}
+//sayMyName()
+
+
+
+function addTwoNums(number1, number2){
+
+}
