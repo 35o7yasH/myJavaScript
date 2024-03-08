41a6f615674d2148a5dbfaa9cfeb5036c2a87729
35o7yasH <95373145+35o7yasH@users.noreply.github.com>
2024-03-08 13:23:10 +0000

    not today maybe

--- 03_basics/03_arrows.js
+++ 03_basics/03_arrows.js
@@ -1,3 +1,13 @@
-const user = { 
-    
-}
+// const user = { 
+//     username: "Yash",
+//     price: 999,
+
+//     welcomemessage: function(){
+//         console.log(`${this.username}, welcome to website.`);
+//     }
+// }
+// console.log(user.welcomemessage);
+
+
+
+console.log(typeof typeof 1);
