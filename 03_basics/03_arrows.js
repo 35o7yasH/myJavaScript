435a4ca077376c3e90609465a01f27be6caedc3a
Yash Karmakar <95373145+35o7yasH@users.noreply.github.com>
2024-03-09 02:37:24 +0000

    this and arrow done

--- 03_basics/03_arrows.js
+++ 03_basics/03_arrows.js
@@ -1,13 +1,66 @@
-// const user = { 
-//     username: "Yash",
-//     price: 999,
+const user = { 
+    username: "Yash",
+    price: 999,
 
-//     welcomemessage: function(){
-//         console.log(`${this.username}, welcome to website.`);
-//     }
+    welcomemessage: function(){
+        console.log(`${this.username}, welcome to website.`);
+        console.log(this);
+    }
+}
+// user.welcomemessage()
+// user.username = "sam"
+// user.welcomemessage()
+
+// console.log(this);
+
+
+
+// function chai(){
+//     let username = "yash"
+//     console.log(this)
 // }
-// console.log(user.welcomemessage);
+// chai()
 
 
 
-console.log(typeof typeof 1);
+// function chai(){
+//     let username = "Yash"
+//     console.log(this.username);
+// }
+// chai()
+
+// const chai = function() {
+//     let username = "Yash"
+//     console.log(this.username);
+// }
+// chai ()
+
+// const chai = () => {
+//     let username = "Yash"
+//     console.log(this.username);
+// }
+// chai()
+
+// const chai = () => {
+//     let username = "Yash"
+//     console.log(this);
+// }
+// chai()
+
+
+// const addtwo = (num1, num2) => {
+//     return num1 + num2
+// }
+// Implicit return
+
+// const addTwo = (num1, num2) => num1 + num2
+//or
+// const addTwo = (num1, num2) => ( num1 + num2 )
+// console.log(addTwo(3, 15));
+
+
+const object = () => ({userName: "Yash"})
+console.log(object());
+
+// const myArray = [2, 4, 5, 9, 7]
+// myArray.forEach()
