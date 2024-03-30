26bc2b8748ebb0d2ef67555bcbad478211f46f72
Yash Karmakar <95373145+35o7yasH@users.noreply.github.com>
2024-03-30 09:00:45 +0000

    to be continued

---
+++ 05_iterations/one.js
@@ -0,0 +1,27 @@
+// for loop
+
+for (let index = 0; index <= 10; index++) {
+    const element = index;
+    if (element == 5) {
+        //console.log(`5 is the best number.`);
+    }
+    //console.log(element);
+    
+}
+
+
+
+for (let i = 0; i <= 10; i++) {
+    //console.log(`Outer loop value is ${i}`);
+    for (let j = 0; j <= 10; j++) {
+        //console.log(`Inner loop value is ${j}, and outer loop value is ${i}`);
+        
+    }
+    
+}
+
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
