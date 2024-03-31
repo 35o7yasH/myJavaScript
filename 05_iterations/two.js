9ca0d020ee51653737ca4cb2a03f6c1db125622b
Yash Karmakar <95373145+35o7yasH@users.noreply.github.com>
2024-03-31 13:24:50 +0000

    while & do-while loops done

---
+++ 05_iterations/two.js
@@ -0,0 +1,35 @@
+// while loops
+
+// Syntax
+// while (condition) {
+    
+// }
+
+
+let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is: ${index}`);
+//     index ++
+// }
+
+let myArray = ["ironman", "thor", "hulk"];
+let arr = 0
+while (arr<myArray.length) {
+    //console.log(`Value is: ${myArray[arr]}`);
+    arr++
+}
+
+
+
+// do-while loop
+
+// Syntax
+// do {
+    
+// } while (condition);
+
+let me = 1
+do {
+    console.log(`Lets see me: ${me}`);
+    me ++
+} while (me <= 10);
