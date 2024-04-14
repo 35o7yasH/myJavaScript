0194e536ed8f45ab7d2567fe1d160542f785b85c
Yash Karmakar <95373145+35o7yasH@users.noreply.github.com>
2024-04-14 08:51:06 +0000

    date and time

--- 01_basics/07_datesinJS.js
+++ 01_basics/07_datesinJS.js
@@ -1,13 +1,14 @@
 //Dates
 
+// Dates in javascript is beginnings from 01/01/1970 & and generally it is calculated in miliseconds
 
 let myDate = new Date();
-// console.log(myDate);
-// console.log(myDate.toString());
+// console.log(myDate); //2024-04-14T08:16:48.849Z
+// console.log(myDate.toString()); //Sun Apr 14 2024 08:17:14 GMT+0000 (Coordinated Universal Time)
 //console.log(myDate.toJSON()); // 2024-03-25T05:36:21:954Z
-// console.log(myDate.toLocaleString());
-// console.log(myDate.toDateString());
-// console.log(typeof myDate);
+// console.log(myDate.toLocaleString()); // 4/14/2024, 8:18:00 AM
+// console.log(myDate.toDateString()); // Sun Apr 14 2024
+// console.log(typeof myDate); // object
 
 
 
