37b17f81a802e1b36c37b61addf27533b00f7fc7
Yash Karmakar <95373145+35o7yasH@users.noreply.github.com>
2024-04-23 17:57:02 +0000

    will be back soon

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -5,3 +5,20 @@
 
 
 
+// ["", "", ""]
+// [{}, {}, {}]
+
+
+// const arr = [1, 2, 3, 4, 5];
+// for (const val of arr) {
+//     console.log(val);
+// }
+
+const greeting = "Hello World!"
+for (const greet of greeting) {
+    console.log(greet);
+}
+
+
+// Maps
+
