0378e10e207485406b1d2550a3616aeae3250cfc
Yash Karmakar <95373145+35o7yasH@users.noreply.github.com>
2024-04-23 19:48:05 +0000

    i'mgetting to know for of loop

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -14,11 +14,36 @@
 //     console.log(val);
 // }
 
-const greeting = "Hello World!"
-for (const greet of greeting) {
-    console.log(greet);
-}
+// const greeting = "Hello World!"
+// for (const greet of greeting) {
+//     if(greet==' '){
+//         continue;
+//     }
+//     console.log(greet);
+// }
 
 
 // Maps
 
+
+const map = new Map()
+map.set("IN", "India")
+map.set("Fr", "France")
+map.set("Ne", "Nepal")
+
+// console.log(map);
+
+// for (const [key, value] of map) {
+//     console.log(key, ":-", value);
+// }
+
+
+// const myObj = {
+//     'game1': 'NFS',
+//     'game2': "Valo",
+//     'game3': 'pubgm'
+// }
+                                            // It doesn't work on objects
+// for (const [key, value] of myObj) {
+//     console.log(key, value);
+// }
