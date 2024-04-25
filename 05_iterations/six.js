1e08c1d134cf8b73c78bb62b2a31741b0d59718c
Yash Karmakar <95373145+35o7yasH@users.noreply.github.com>
2024-04-25 09:59:16 +0000

    wait

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -14,10 +14,14 @@
 
 
 
+// const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const newNums =  myNums.filter( (num)=> num > 4)       
+
+// console.log(newNums);
+
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const newNums =  myNums.filter( (num)=> num > 4)
+const newNums = myNums.filter( (num)=>{
+    return num > 4
+} )
 
 console.log(newNums);
-
-
-
