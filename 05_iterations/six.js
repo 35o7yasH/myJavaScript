8e5c2f3441c007e408a08dd602b4cb47105d44e2
Yash Karmakar <95373145+35o7yasH@users.noreply.github.com>
2024-04-25 06:59:22 +0000

    don't feel like to complete now, but will be back

---
+++ 05_iterations/six.js
@@ -0,0 +1,23 @@
+// const coding = ["javaScript", "python", "rust", "golang", "java", "cpp"];
+
+// coding.forEach( (item)=>{
+//     console.log(item);
+// } )
+
+// const languages = coding.forEach( (item)=>{           // In forEach too the execution happens but it doesn't return you the value
+//     console.log(item);
+//     return item;
+// } )
+
+// console.log(languages);
+
+
+
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const newNums =  myNums.filter( (num)=> num > 4)
+
+console.log(newNums);
+
+
+
