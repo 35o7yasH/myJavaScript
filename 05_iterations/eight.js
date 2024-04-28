6bb69234226d4ebc7d0d56ccd0eaf5c1db437052
Yash Karmakar <95373145+35o7yasH@users.noreply.github.com>
2024-04-28 20:16:33 +0000

    not finished omg

--- 05_iterations/eight.js
+++ 05_iterations/eight.js
@@ -1,5 +1,6 @@
 const myNums = [1, 2, 3]
 const initialValues = 0;
 const newNums = myNums.reduce( 
-    (acc, curval)
-)
+    (acc, curval) => acc + curval + initialValues
+);
+console.log(newNums);
