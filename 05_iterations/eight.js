7ba5ad14e464570196dc3921964513b1816ce09c
Yash Karmakar <95373145+35o7yasH@users.noreply.github.com>
2024-04-28 19:00:22 +0000

    will be back

---
+++ 05_iterations/eight.js
@@ -0,0 +1,5 @@
+const myNums = [1, 2, 3]
+const initialValues = 0;
+const newNums = myNums.reduce( 
+    (acc, curval)
+)
--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -1,3 +1,20 @@
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const newNums = myNums.map( (num)=> num+10 )
+// const newNums = myNums.map( (num)=> {return num+10} )
+// console.log(newNums);
+
+
+// const newNums = []
+// myNums.forEach( (nums)=>{
+//     newNums.push(nums +10)
+// })
+// console.log(newNums);
+
+
+
+
+const newNums = myNums
+                .map( (num)=> num * 10)
+                .map( (num)=> num +1)
+                .filter( (num)=> num >= 40)
+
 console.log(newNums);
