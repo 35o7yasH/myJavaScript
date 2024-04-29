16be4e977109fce4c556ca578ac615f91069be58
Yash Karmakar <95373145+35o7yasH@users.noreply.github.com>
2024-04-29 16:07:57 +0000

    done

--- 05_iterations/eight.js
+++ 05_iterations/eight.js
@@ -1,6 +1,41 @@
-const myNums = [1, 2, 3]
-const initialValues = 0;
-const newNums = myNums.reduce( 
-    (acc, curval) => acc + curval + initialValues
-);
-console.log(newNums);
+// const myNums = [1, 2, 3]
+// const initialValues = 0;
+// const newNums = myNums.reduce( 
+//     (acc, curval) => acc + curval + initialValues
+// );
+// console.log(newNums);
+
+
+
+const myNums = [1, 2, 3];
+const myTotal = myNums.reduce( function(acc, curval) {
+    console.log(`acc: ${acc}, curval: ${curval}`);
+    return acc + curval
+}, 0)
+console.log(myTotal);
+
+
+
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceIfBuyCourse = shoppingCart.reduce( (acc, item)=> acc + item.price, 0)
+console.log(priceIfBuyCourse);
