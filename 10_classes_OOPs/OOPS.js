f5ead123da847efff2910daa3c6ee7983c1b0f32
Yash Karmakar <95373145+35o7yasH@users.noreply.github.com>
2024-07-02 10:10:39 +0000

    not finished yet

---
+++ 10_classes_OOPs/OOPS.js
@@ -0,0 +1,20 @@
+const user = {
+    username: "Yash",
+    loginCount: 8,
+    signedInfo: true,
+
+
+
+
+    getUserDetails: function(){
+        // console.log("Got user details from the database");
+        // console.log(`Username: ${this.username}`);
+        console.log(this);
+    }
+}
+
+// console.log(user.username)
+// console.log(user.getUserDetails());
+// console.log(this);
+
+
---
+++ 10_classes_OOPs/notes.md
@@ -0,0 +1,27 @@
+# JavaScript and Classes.
+
+# OOPs
+
+## Objects:-
+- Collections of various properties and methods.
+- eg. toLoweCase
+
+## Parts of OOP
+
+Object Literals
+
+- Constructor functions
+- Prototypes / proto
+- Classes
+- Instances (new, this)
+
+
+## 4 Pillars
+
+* Abstraction
+
+* Encapsulation
+
+* Inheritance
+
+* Polymorphism
